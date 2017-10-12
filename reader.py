d6591989844dd39034ca03feef565a60284bd216
David <davidapgilman@gmail.com>
2017-10-12 18:46:27 -0500

    Added output to screen

--- reader.py
+++ reader.py
@@ -1,6 +1,5 @@
 #remote - - [13/Feb/1995:10:48:26 -0700] "GET index.html HTTP/1.0" 200 9902
 #local - - [24/Oct/1994:13:41:41 -0600] "GET index.html HTTP/1.0" 200 150
-import urllib.request
 import datetime
 
 while True:
@@ -8,6 +7,7 @@ while True:
 		fo = open("http_access_log", "r");
 		break
 	except IOError:
+		import urllib.request
 		urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
 
 requests = 0
@@ -15,6 +15,7 @@ unsuccess = 0
 redirect = 0
 log = []
 files = {}
+
 for line in fo:
 	try:
 		if line.split()[8][0] == "4": unsuccess+=1
@@ -27,6 +28,10 @@ for line in fo:
 		pass
 #	break
 #	log.append(line[11:37],)
+print("Total valid requests: " + str(requests))
+print("Unsucesful requests: " + str(unsuccess))
+print("Redirected requests: " + str(redirect))
+print("Most requested file: " + max(files, key=files.get))
+print("Least requested file: " + min(files, key=files.get))
 
-print(requests)
 fo.close()
