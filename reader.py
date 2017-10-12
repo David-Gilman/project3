629694e445d3f309bddf6380a60443d570a0b1f4
David <davidapgilman@gmail.com>
2017-10-12 17:06:42 -0500

    Error handling

--- reader.py
+++ reader.py
@@ -1,8 +1,19 @@
-fo = open("http_access_log", "r");
+import urllib.request
+import datetime
+
+while True:
+	try:
+		fo = open("http_access_log", "r");
+		break
+	except IOError:
+		urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
 
 requests = 0
+log = []
+files = {}
 for line in fo:
-	requests+=1
-	print(line)
-print (requests)
+	log.append(line[11:37],)
+	
+
+print(log[0])
 fo.close()
