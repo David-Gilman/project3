469c846538b035406f7b55d7a7e7c97ab2b5387b
David <davidapgilman@gmail.com>
2017-10-17 17:33:08 -0500

    Added main method and got datetime working

--- reader.py
+++ reader.py
@@ -1,54 +1,65 @@
-#iremote - - [13/Feb/1995:10:48:26 -0700] "GET index.html HTTP/1.0" 200 9902
-import datetime
-import re
+# iremote - - [13/Feb/1995:10:48:26 -0700] "GET index.html HTTP/1.0" 200 9902
 import calendar
-import time
-
-while True:
-	try:
-		fo = open("http_access_log", "r");
-		print("Searching log...")
-		break
-	except IOError:
-		import urllib.request
-		print("Log not found")
-		print("Downloading log...")
-		urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
-		print("Log retrieved")
-
-requests = 0
-unsuccess = 0
-redirect = 0
-weeks = 0
-months = 0
-files = {}
-weekday = {}
-for i in range(0,7):
-	weekday[i] = 0
-conv = dict((v,k) for k,v in enumerate(calendar.month_abbr)) 
-reg = re.compile('.*\[([^:]*):(.*) \-\d{3,4}\].*')
-
-for line in fo:
-#	try:
-		if line.split()[8][0] == "4": unsuccess+=1
-		elif line.split()[8][0] == "3": redirect+=1 
-		if line.split()[6] in files: files[line.split()[6]]+=1
-		else: files[line.split()[6]] = 1
-		requests+=1
-		#event = time.strptime(reg.match(line))
-		print(re.split('[/ :]',(line.split()[3])))
-		event = datetime(int((re.split('[/ :]',line.split()[3])[2])),conv[re.split('[/ :]',(line.split()[3])[1])][0],int((re.split('[/ :]',line.split()[0])[2])))
-		print(event)
-		weekday[event.weekday()] = weekday[event.weekday()] + 1 
-	#except:
-#		pass
+import re
+from datetime import datetime
 
 
-print("Total requests: " + str(requests))
-print("Unsucesful requests: " + str(round((unsuccess/requests),4)*100) + "%")
-print("Redirected requests: " + str(round((redirect/requests),4)*100) + "%")
-print("Most requested file: " + max(files, key=files.get))
-print("Least requested file: " + min(files, key=files.get))
-print(weekday)
+def main():
+    while True:
+        try:
+            fo = open("http_access_log", "r")
+            print("Searching log...")
+            break
+        except IOError:
+            import urllib.request
 
-fo.close()
+            print("Log not found")
+            print("Downloading log...")
+            urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
+            print("Log retrieved")
+
+    requests = 0
+    unsuccess = 0
+    redirect = 0
+    weeks = 0
+    months = 0
+    files = {}
+    weekday = {}
+    for i in range(0, 7):
+        weekday[i] = 0
+    conv = dict((v, k) for k, v in enumerate(calendar.month_abbr))
+    reg = re.compile('.*\[([^:]*):(.*) -\d{3,4}\].*')
+    bad = 0
+
+    for line in fo:
+        try:
+            if line.split()[8][0] == "4":
+                unsuccess += 1
+            elif line.split()[8][0] == "3":
+                redirect += 1
+            if line.split()[6] in files:
+                files[line.split()[6]] += 1
+            else:
+                files[line.split()[6]] = 1
+            requests += 1
+            event = datetime.strptime(reg.match(line).groups()[0], '%d/%b/%Y')
+            # print(re.split('[/ :]',(line.split()[3])))
+            # event = datetime(int((re.split('[/ :]',line.split()[3])[2])),conv[re.split('[/ :]',(line.split()[3])[1])][0],int((re.split('[/ :]',line.split()[0])[2])))
+            weekday[event.weekday()] = weekday[event.weekday()] + 1
+        except IndexError:
+            bad += 1
+            pass
+
+    print("Total requests: " + str(requests))
+    print("Unsucesful requests: " + str(round((unsuccess / requests), 4) * 100) + "%")
+    print("Redirected requests: " + str(round((redirect / requests), 4) * 100) + "%")
+    print("Most requested file: " + max(files, key=files.get))
+    print("Least requested file: " + min(files, key=files.get))
+    print("Invalid log entries: " + bad + "representing " + (bad / (bad + requests)))
+    print(weekday)
+
+    fo.close()
+
+
+if __name__ == '__main__':
+    main()
