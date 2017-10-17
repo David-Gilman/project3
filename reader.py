d3bd217bf800838abdd88ca34f4d1beb55de95b7
David <davidapgilman@gmail.com>
2017-10-17 18:17:37 -0500

    Cleaned up output

--- reader.py
+++ reader.py
@@ -2,7 +2,7 @@
 import calendar
 import re
 from datetime import datetime
-
+from timeit import default_timer as timer
 
 def main():
     while True:
@@ -12,29 +12,25 @@ def main():
             break
         except IOError:
             import urllib.request
-
             print("Log not found")
             print("Downloading log...")
             urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
             print("Log retrieved")
 
-    requests = 0
-    unsuccess = 0
-    redirect = 0
-    weeks = 0
-    months = 0
-    files = {}
-    weekday = {}
-    for i in range(0, 7):
-        weekday[i] = 0
+    start = timer()
+    requests, fail, redirect, bad = 0, 0, 0, 0
+    files, weekday, week, month = {}, {}, {}, {}
+    for i in range(0, 7): weekday[i] = 0
+    for i in range(0, 52): month[i] = 0
+    for i in range(0, 12): month[i] = 0
+    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
     conv = dict((v, k) for k, v in enumerate(calendar.month_abbr))
     reg = re.compile('.*\[([^:]*):(.*) -\d{3,4}\].*')
-    bad = 0
 
     for line in fo:
         try:
             if line.split()[8][0] == "4":
-                unsuccess += 1
+                fail += 1
             elif line.split()[8][0] == "3":
                 redirect += 1
             if line.split()[6] in files:
@@ -43,20 +39,23 @@ def main():
                 files[line.split()[6]] = 1
             requests += 1
             event = datetime.strptime(reg.match(line).groups()[0], '%d/%b/%Y')
-            # print(re.split('[/ :]',(line.split()[3])))
-            # event = datetime(int((re.split('[/ :]',line.split()[3])[2])),conv[re.split('[/ :]',(line.split()[3])[1])][0],int((re.split('[/ :]',line.split()[0])[2])))
             weekday[event.weekday()] = weekday[event.weekday()] + 1
         except IndexError:
             bad += 1
             pass
 
-    print("Total requests: " + str(requests))
-    print("Unsucesful requests: " + str(round((unsuccess / requests), 4) * 100) + "%")
+    end = timer()
+    print(str(round(end - start)) + " Seconds for processing")
+    print("")
+    print("1. Total requests: " + str(requests))
+    print("2. Requests per day: ", )
+    for i in range(7):
+        print('\t' + str(days[i]) + ': ' + str(weekday[i]))
+    print("Unsuccessful requests: " + str(round((fail / requests), 4) * 100) + "%")
     print("Redirected requests: " + str(round((redirect / requests), 4) * 100) + "%")
     print("Most requested file: " + max(files, key=files.get))
-    print("Least requested file: " + min(files, key=files.get))
-    print("Invalid log entries: " + bad + "representing " + (bad / (bad + requests)))
-    print(weekday)
+    # print("Least requested file: " + min(files, key=files.get))
+    print("Invalid log entries: " + str(bad) + " representing " + str(round((bad / (bad + requests)), 4) * 100) + "%")
 
     fo.close()
 
