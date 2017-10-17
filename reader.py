24b0ca5dd0f29d5bf8ecc27112a5f21d5d26723c
David <davidapgilman@gmail.com>
2017-10-17 18:45:49 -0500

    Continuing cleaning up output and code formatting. Added month output

--- reader.py
+++ reader.py
@@ -1,8 +1,8 @@
-# iremote - - [13/Feb/1995:10:48:26 -0700] "GET index.html HTTP/1.0" 200 9902
 import calendar
 import re
 from datetime import datetime
 from timeit import default_timer as timer
+
 
 def main():
     while True:
@@ -15,15 +15,17 @@ def main():
             print("Log not found")
             print("Downloading log...")
             urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
-            print("Log retrieved")
+            print("Log retrieved\n")
 
     start = timer()
     requests, fail, redirect, bad = 0, 0, 0, 0
     files, weekday, week, month = {}, {}, {}, {}
-    for i in range(0, 7): weekday[i] = 0
-    for i in range(0, 52): month[i] = 0
-    for i in range(0, 12): month[i] = 0
-    days = ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday', 'Sunday']
+    for i in range(0, 7):
+        weekday[i] = 0
+    for i in range(0, 52):
+        week[i] = 0
+    for i in range(0, 12):
+        month[i] = 0
     conv = dict((v, k) for k, v in enumerate(calendar.month_abbr))
     reg = re.compile('.*\[([^:]*):(.*) -\d{3,4}\].*')
 
@@ -39,7 +41,11 @@ def main():
                 files[line.split()[6]] = 1
             requests += 1
             event = datetime.strptime(reg.match(line).groups()[0], '%d/%b/%Y')
-            weekday[event.weekday()] = weekday[event.weekday()] + 1
+            weekday[event.weekday()] += 1
+            week[event.isocalendar()[1] - 1] += 1
+            month[event.month - 1] += 1
+            
+
         except IndexError:
             bad += 1
             pass
@@ -47,18 +53,26 @@ def main():
     end = timer()
     print(str(round(end - start)) + " Seconds for processing")
     print("")
-    print("1. Total requests: " + str(requests))
-    print("2. Requests per day: ", )
-    for i in range(7):
-        print('\t' + str(days[i]) + ': ' + str(weekday[i]))
-    print("Unsuccessful requests: " + str(round((fail / requests), 4) * 100) + "%")
-    print("Redirected requests: " + str(round((redirect / requests), 4) * 100) + "%")
-    print("Most requested file: " + max(files, key=files.get))
-    # print("Least requested file: " + min(files, key=files.get))
     print("Invalid log entries: " + str(bad) + " representing " + str(round((bad / (bad + requests)), 4) * 100) + "%")
+    print("")
+    print("1. Total requests: " + str(requests))
+    print("2. Requests per day: ")
+    for i in range(7):
+        print('\t' + str(calendar.day_abbr[i]) + ': ' + str(weekday[i]))
+    print("Requests per week are saved in json file \"weeks.JSON\"")
+    print("Requests per month: ")
+    for i in range(12):
+        print('\t' + str(calendar.month_abbr[i + 1]) + ': ' + str(month[i]))
+    print("3. Unsuccessful requests: " + str(round((fail / requests), 4) * 100) + "%")
+    print("4. Redirected requests: " + str(round((redirect / requests), 4) * 100) + "%")
+    print("5. Most requested file: " + max(files, key=files.get))
+    print("6. Files requested only once are stored in orphans.txt")
+    print("See this directory for 12 .log files, each storing the data for the proper month")
+    # print("Least requested file: " + min(files, key=files.get))
 
     fo.close()
 
 
 if __name__ == '__main__':
     main()
+# iremote - - [13/Feb/1995:10:48:26 -0700] "GET index.html HTTP/1.0" 200 9902
