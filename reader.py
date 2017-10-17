cd57ee45c66f339cdd5f1a8833bbcaf01f109b91
David <davidapgilman@gmail.com>
2017-10-16 21:37:33 -0500

    Attemping to get date working

--- reader.py
+++ reader.py
@@ -1,14 +1,19 @@
 #iremote - - [13/Feb/1995:10:48:26 -0700] "GET index.html HTTP/1.0" 200 9902
 import datetime
 import re
+import calendar
 
 while True:
 	try:
 		fo = open("http_access_log", "r");
+		print("Searching log...")
 		break
 	except IOError:
 		import urllib.request
+		print("Log not found")
+		print("Downloading log...")
 		urllib.request.urlretrieve("https://s3.amazonaws.com/tcmg412-fall2016/http_access_log", "http_access_log")
+		print("Log retrieved")
 
 requests = 0
 unsuccess = 0
@@ -19,6 +24,7 @@ files = {}
 weekday = {}
 for i in range(0,7):
 	weekday[i] = 0
+conv = {v: k for k,v in enumerate(calendar.month_abbr)}
 
 for line in fo:
 	try:
@@ -27,15 +33,16 @@ for line in fo:
 		if line.split()[6] in files: files[line.split()[6]]+=1
 		else: files[line.split()[6]] = 1
 		requests+=1
-		date = datetime.datetime(int(line.split()[3].re.split('/:')[3]))
-		weekday[date.weekday()] = files[date.weekday()] + 1  
+		event = datetime(int((re.split('[/ :]',line.split()[3])[2])),conv[re.split('[/ :]',(line.split()[3])[1])],int((re.split('[/ :]',line.split()[0])[2])))
+		print(event)
+		weekday[event.weekday()] = weekday[event.weekday()] + 1 
 	except:
 		pass
 
 
 print("Total requests: " + str(requests))
 print("Unsucesful requests: " + str(round((unsuccess/requests),4)*100) + "%")
-print("Redirected requests: " + str(redirect))
+print("Redirected requests: " + str(round((redirect/requests),4)*100) + "%")
 print("Most requested file: " + max(files, key=files.get))
 print("Least requested file: " + min(files, key=files.get))
 print(weekday)
